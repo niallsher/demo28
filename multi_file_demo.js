de664eeff11415e9f80a775d3ad6e3824c945d4f
niallsher <78381666+niallsher@users.noreply.github.com>
2021-04-28 18:22:46 -0400

    Add files via upload

---
+++ multi_file_demo.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+	
+http.createServer(function (req, res) 
+{
+	  
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("This is the home page<br>");
+	res.end();
+  
+
+}).listen(port);
